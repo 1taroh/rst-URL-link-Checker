99cb160517c7893d21ce7f3c32bff82010428409
1taroh <65810214+1taroh@users.noreply.github.com>
2023-08-16 22:00:22 +0900

    add line number

--- test/test_links.py
+++ test/test_links.py
@@ -1,21 +1,27 @@
 import re
 
-def extract_embedded_links(file_path):
+def extract_embedded_links_with_line_numbers(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
+        content = file.readlines()
 
-    # 正規表現パターンを定義
     link_pattern = re.compile(r'`([^`]+) <([^`]+)>`_')
 
-    # マッチする全てのリンクを抽出
-    matches = link_pattern.findall(content)
+    embedded_links = []
 
-    return matches
+    for line_number, line in enumerate(content, start=1):
+        matches = link_pattern.finditer(line)
+        for match in matches:
+            link_text = match.group(1)
+            link_url = match.group(2)
+            embedded_links.append((link_text, link_url, line_number))
 
-rst_file_path = 'rstfiles/source/index.rst' #TODO: ディレクトリを渡してその下のrstファイルを抽出する
-embedded_links = extract_embedded_links(rst_file_path)
+    return embedded_links
 
-for link_text, link_url in embedded_links:
+rst_file_path = 'rstfiles/source/index.rst'
+embedded_links = extract_embedded_links_with_line_numbers(rst_file_path)
+
+for link_text, link_url, line_number in embedded_links:
     print(f"Link Text: {link_text}")
     print(f"Link URL: {link_url}")
+    print(f"Line Number: {line_number}")
     print("-" * 20)
