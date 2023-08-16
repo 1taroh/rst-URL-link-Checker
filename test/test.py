99f766d91a64879e140737d3dd77f6e0cc29d996
1taroh <65810214+1taroh@users.noreply.github.com>
2023-08-16 20:32:42 +0900

    add an output test

---
+++ test/test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import time
+
+data = [["hoge.rst",18,"google.com",200], 
+        ["fuga.rst",23,"duckduckgo.com",200], 
+        ["foo.rst",30,"mikamika.io",404]]
+
+df = pd.DataFrame(data)
+df.columns = ["rst fime name", "line", "URL", "http status"]
+
+print(df)
+
+print("Do you want to save this list? y/n")
+if(input()=="y"):
+    name = time.strftime('%Y%m%d_%H:%M:%S')
+    df.to_csv("data.csv")
